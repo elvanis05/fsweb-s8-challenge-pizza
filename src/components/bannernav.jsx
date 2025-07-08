0a0537cbd0a5c16f3be1e57f0be07272ac840a6b
Elvan <elvanus05@gmail.com>
2025-07-08 17:32:36 +0300

    s

--- src/components/bannernav.jsx
+++ src/components/bannernav.jsx
@@ -7,23 +7,23 @@ return(
               <span>YENİ! Kore</span>
             </a>
             <a href="">
-              <img src="../images/iteration-2-images/icons/2.svg" alt="Pizza"/>
+              <img src="/images/iteration-2-images/icons/2.svg" alt="Pizza"/>
               <span>Pizza</span>
             </a>
             <a href="">
-              <img src="../images/iteration-2-images/icons/3.svg" alt="Burger"/>
+              <img src="/images/iteration-2-images/icons/3.svg" alt="Burger"/>
               <span>Burger</span>
             </a>
             <a href="">
-              <img src="../images/iteration-2-images/icons/4.svg" alt="Kızartmalar"/>
+              <img src="/images/iteration-2-images/icons/4.svg" alt="Kızartmalar"/>
               <span>Kızartmalar</span>
             </a>
             <a href="">
-              <img src="../images/iteration-2-images/icons/5.svg" alt="Fast food"/>
+              <img src="/images/iteration-2-images/icons/5.svg" alt="Fast food"/>
               <span>Fast food</span>
             </a>
             <a href="">
-              <img src="../images/iteration-2-images/icons/6.svg" alt="Gazlı İçecek"/>
+              <img src="/images/iteration-2-images/icons/6.svg" alt="Gazlı İçecek"/>
               <span>Gazlı İçecek</span>
             </a>
           </nav>
